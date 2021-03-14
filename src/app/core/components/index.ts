8524b6895d9ab3fa37e2cf4b90454f104162fbe4
artsper <thomas.lenoel@artsper.com>
2021-03-14 17:15:51 +0100

    Add footer component - Fix tests [DONE]

--- src/app/core/components/index.ts
+++ src/app/core/components/index.ts
@@ -1,9 +1,11 @@
 import { HeaderComponent } from '@app/core/components/header/header.component';
 import { SidebarComponent } from '@app/core/components/sidebar/sidebar.component';
 import { SidebarAccordionComponent } from '@app/core/components/sidebar-accordion/sidebar-accordion.component';
+import { FooterComponent } from '@app/core/components/footer/footer.component';
 
 export const components = [
   HeaderComponent,
   SidebarComponent,
-  SidebarAccordionComponent
+  SidebarAccordionComponent,
+  FooterComponent
 ];
