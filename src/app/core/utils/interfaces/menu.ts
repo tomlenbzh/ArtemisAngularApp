1d52a8d8f6ced69d85c98eaca2531acf600e9a8f
artsper <thomas.lenoel@artsper.com>
2021-03-14 18:44:43 +0100

    remove unnecessary comments

--- src/app/core/utils/interfaces/menu.ts
+++ src/app/core/utils/interfaces/menu.ts
@@ -1,34 +1,5 @@
 import { LANGUAGES } from '../constants/languages';
 
-// export interface MenuLogo {
-//   large: string;
-//   small: string;
-// }
-
-// export interface NavItem {
-//   label: string;
-//   route?: string;
-//   children?: NavItem[];
-// }
-
-// export interface LanguageItem {
-//   label: string;
-//   lang: LANGUAGES;
-// }
-
-// export interface SocialNetworkItem {
-//   label: string;
-//   link: string;
-// }
-
-// export interface Menu {
-//   logo: MenuLogo;
-//   items: NavItem[];
-//   languages?: LanguageItem[];
-//   social?: SocialNetworkItem[];
-// }
-
-
 /* ****************************** */
 
 export interface MenuItem {
